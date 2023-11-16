24ca3983759fdf7ffd00510f00f806b50192cfe3
jf <jpf0628@gmail.com>
2023-11-15 20:51:32 -0800

    add NavDropdown wip: Header. Cannot implement NavDropdown w/o Header

---
+++ src/NavDropdown.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { HashLink } from "react-router-hash-link";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import './NavDropdown.css'
+function NavDropdown() {
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant='dark-outline' split={false} id="dropdown-basic">
+        <FontAwesomeIcon icon={faBars} />
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu style={{ background: "transparent" }}>
+        {/* <Dropdown.Menu variant="success"> */}
+        <Dropdown.Item style={{ background: "transparent" }}><HashLink className="Nav-font" to="#Bio-about-me">About</HashLink></Dropdown.Item>
+        <Dropdown.Item style={{ background: "transparent" }}><Link className="Nav-font" to="/portfolio">Portfolio</Link></Dropdown.Item>
+        <Dropdown.Item style={{ background: "transparent" }}>      <a className="Nav-font"
+          href="https://github.com/JoHeyJo/myWebsiteReact/raw/main/public/Joannes_Figueroa_Resume.pdf"
+        >Resume</a></Dropdown.Item>
+        <Dropdown.Item style={{ background: "transparent" }}><HashLink className="Nav-font" to="#footer-content">Contact</HashLink></Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+export default NavDropdown;
