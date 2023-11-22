8912b623689483702312c6f74b41a5ad669216a4
jf <jpf0628@gmail.com>
2023-11-22 12:32:26 -0800

    NavDropdown house keeping

--- src/NavDropdown.tsx
+++ src/NavDropdown.tsx
@@ -5,20 +5,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import './styles/NavDropdown.css'
 
+const RESUME_LINK = "https://github.com/JoHeyJo/myWebsiteReact/raw/main/public/Joannes_Figueroa_Resume.pdf"
+
 function NavDropdown() {
   return (
     <Dropdown>
       <Dropdown.Toggle variant='dark-outline' split={false} id="dropdown-basic">
         <FontAwesomeIcon icon={faBars} />
       </Dropdown.Toggle>
-      <Dropdown.Menu style={{ background: "transparent" }}>
-        {/* <Dropdown.Menu variant="success"> */}
-        <Dropdown.Item><HashLink className="Nav-font" to="#Bio-about-me">About</HashLink></Dropdown.Item>
-        <Dropdown.Item><Link className="Nav-font" to="/portfolio">Portfolio</Link></Dropdown.Item>
-        <Dropdown.Item>      <a className="Nav-font"
-          href="https://github.com/JoHeyJo/myWebsiteReact/raw/main/public/Joannes_Figueroa_Resume.pdf"
-        >Resume</a></Dropdown.Item>
-        <Dropdown.Item><HashLink className="Nav-font" to="#footer-content">Contact</HashLink></Dropdown.Item>
+      <Dropdown.Menu>
+        <Dropdown.Item>
+          <HashLink className="Nav-font" to="#Bio-about-me">About</HashLink>
+        </Dropdown.Item>
+        <Dropdown.Item>
+          <Link className="Nav-font" to="/portfolio">Portfolio</Link>
+        </Dropdown.Item>
+        <Dropdown.Item><a className="Nav-font" href={RESUME_LINK}>Resume</a>
+        </Dropdown.Item>
+        <Dropdown.Item>
+          <HashLink className="Nav-font" to="#footer-content">Contact</HashLink>
+        </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
