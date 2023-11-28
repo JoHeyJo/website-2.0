57a0adc53a323e7880921d8cb1c9a5b615ba46ae
jf <jpf0628@gmail.com>
2023-11-28 13:17:23 -0800

    fixed bug that allows carousel to switch between items

--- src/PortfolioCarousel.tsx
+++ src/PortfolioCarousel.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { SyntheticEvent, useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import Project from './Project';
 import { Project as ProjectType } from './utils/interfaces';
@@ -13,20 +13,20 @@ type PortfolioCarouselProp = {
  * Portfolio -> PortfolioCarousel -> Project
 */
 function PortfolioCarousel({ isRendering, projects }: PortfolioCarouselProp) {
-  const [index, setIndex] = useState(2);
+  const [index, setIndex] = useState(0);
 
-  const handleSelect = (selectedIndex: number, e: any) => {
+  const handleSelect = (selectedIndex: number) => {
     setIndex(selectedIndex);
   };
 
   return (
     <>
-      <Carousel interval={null} activeIndex={index}>
+      <Carousel interval={null} activeIndex={index} onSelect={handleSelect}>
         {projects.map(project =>
           <Carousel.Item>
             <Project isRenderingCarousel={isRendering} project={project} />
-              <h3>{project.heading}</h3>
-              <p>{project.description}</p>
+            <h3>{project.heading}</h3>
+            <p>{project.description}</p>
           </Carousel.Item>
         )}
       </Carousel>
