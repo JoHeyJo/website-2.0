80fa4beae8eb050740c66ab292974d2bc93b9485
jf <jpf0628@gmail.com>
2023-11-30 12:18:22 -0800

    added ContactForm template

--- src/ContactForm.tsx
+++ src/ContactForm.tsx
@@ -1,22 +1,95 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useState } from 'react';
+import Button from "react-bootstrap/Button";
+
+const contact_form = {
+  from_name: '',
+  reply_to: '',
+  to_name: 'JPF0628@gmail.com',
+  subject: '',
+  message: ''
+}
 
 export const ContactForm = () => {
-  const form: any = useRef();
+  const [form, setForm] = useState(contact_form);
+  const [alert, setAlert] = useState(null)
 
-  const sendEmail = (e:any) => {
+  function handleChange(e: any) {
+    e.preventDefault();
+
+    const { name, value } = e.target
+    setForm(formData => ({ ...formData, [name]: value }))
+  }
+
+  async function sendEmail(e: any) {
     e.preventDefault();
   }
+
+  function clearForm() { setForm(contact_form) }
+
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <section id='contact-container'>
+      <form onSubmit={sendEmail}>
+        <div id='greeting-text'>Get in touch!</div>
+
+        <div className='form-inputs'>
+          {/* <label className='sender-info' htmlFor="sender-name"></label> */}
+          <input
+            className='sender-info'
+            onChange={handleChange}
+            type="text"
+            name="from_name"
+            value={form.from_name}
+            placeholder='Name:' />
+        </div>
+
+        <div className='form-inputs'>
+          <select required
+            id='subject'
+            name='subject'
+            className='sender-info'
+            onChange={handleChange}>
+            {form.subject === '' ? <option value='' disabled selected hidden>What is this about:</option> : ''}
+            <option value='' disabled selected hidden>What is this about:</option>
+            <option value='employment'>Job Opportunity</option>
+            <option value='freelance'>Freelance</option>
+            <option value='inquiry'>Talk shop</option>
+            <option value='bug'>Report an issue</option>
+            <option value='other'>Just wanted to say hi!</option>
+          </select>
+        </div>
+
+        <div className='form-inputs'>
+          {/* <label className='sender-info' htmlFor='email-input'></label> */}
+          <input
+            className='sender-info'
+            onChange={handleChange}
+            type="email"
+            name="reply_to"
+            value={form.reply_to}
+            placeholder='Email:' />
+        </div>
+
+        <div className='form-inputs'>
+          <label htmlFor='message-input'></label>
+          <textarea
+            placeholder='Would love to hear your thoughts...'
+            rows={6}
+            onChange={handleChange}
+            name="message"
+            value={form.message} />
+          {/* using Button component submits the wrong data format. error:
+          "The 3rd parameter is expected to be the HTML form element or the style selector of form"
+          even if the form is passed JSEmail function doesn't accept it. */}
+          <input id='submit-button' type="submit" value="Send" />
+          <Button className='m-2' variant="outline-dark" onClick={clearForm}>Clear</Button>
+        </div>
+        {alert &&
+          <div className="alert alert-dark" role="alert">
+            {alert}
+          </div>
+        }
+      </form>
+    </section>
   );
 };
 
