34cbe2ea2ba490c4e37330ea332fd7f5a36bf498
jf <jpf0628@gmail.com>
2023-12-03 21:36:28 -0800

    fixed styles for mobile view on Contact

--- src/ContactForm.tsx
+++ src/ContactForm.tsx
@@ -28,65 +28,65 @@ export const ContactForm = () => {
   function clearForm() { setForm(contact_form) }
 
   return (
-    <Row>
-      <Col md={12}>
-    <form onSubmit={sendEmail}>
-      <div id="ContactForm-user-interface" className="d-flex flex-column ">
-        <div id='greeting-text'>Get in touch!</div>
-        <div id="ContactForm-user-control" className="d-flex flex-column">
-          <input
-            id="ContactForm-user-input"
-            onChange={handleChange}
-            type="text"
-            name="fromName"
-            value={form.fromName}
-            placeholder='Name:' />
+    <Row style={{height: "100%"}}>
+      <Col xs={12} md={12} lg={7} className='d-flex flex-column justify-content-center'>
+        <form onSubmit={sendEmail}>
+          <div id="ContactForm-user-interface" className="d-flex flex-column ">
+            <div id='greeting-text'>Get in touch!</div>
+            <div id="ContactForm-user-control" className="d-flex flex-column">
+              <input
+                id="ContactForm-user-input"
+                onChange={handleChange}
+                type="text"
+                name="fromName"
+                value={form.fromName}
+                placeholder='Name:' />
 
-          <select required
-            id='ContactForm-select'
-            name='subject'
-            onChange={handleChange}>
-            {form.subject === '' ? <option value='' disabled selected hidden>What is this about:</option> : ''}
-            <option value='' disabled selected hidden>What is this about:</option>
-            <option value='employment'>Job Opportunity</option>
-            <option value='freelance'>Freelance</option>
-            <option value='inquiry'>Talk shop</option>
-            <option value='bug'>Report an issue</option>
-            <option value='other'>Just wanted to say hi!</option>
-          </select>
+              <select required
+                id='ContactForm-select'
+                name='subject'
+                onChange={handleChange}>
+                {form.subject === '' ? <option value='' disabled selected hidden>What is this about:</option> : ''}
+                <option value='' disabled selected hidden>What is this about:</option>
+                <option value='employment'>Job Opportunity</option>
+                <option value='freelance'>Freelance</option>
+                <option value='inquiry'>Talk shop</option>
+                <option value='bug'>Report an issue</option>
+                <option value='other'>Just wanted to say hi!</option>
+              </select>
 
-          <input
-            id='ContactForm-email'
-            onChange={handleChange}
-            type="email"
-            name="replyTo"
-            value={form.replyTo}
-            placeholder='Email:' />
+              <input
+                id='ContactForm-email'
+                onChange={handleChange}
+                type="email"
+                name="replyTo"
+                value={form.replyTo}
+                placeholder='Email:' />
 
-          <textarea
-            id='ContactForm-textarea'
-            placeholder='Would love to hear your thoughts...'
-            rows={6}
-            onChange={handleChange}
-            name="message"
-            value={form.message} />
-        </div>
-        {/* using Button component submits the wrong data format. error:
+              <textarea
+                id='ContactForm-textarea'
+                placeholder='Would love to hear your thoughts...'
+                rows={6}
+                onChange={handleChange}
+                name="message"
+                value={form.message} />
+            </div>
+            {/* using Button component submits the wrong data format. error:
           "The 3rd parameter is expected to be the HTML form element or the style selector of form"
         even if the form is passed JSEmail function doesn't accept it. */}
-        <div className='d-flex align-items-baseline'>
-          <input id='ContactForm-submit-button' type="submit" value="Send" />
-          <Button className='m-2' variant="outline-dark" onClick={clearForm}>Clear</Button>
-        </div>
-        {alert &&
-          <div className="alert alert-dark" role="alert">
-            {alert}
+            <div className='d-flex align-items-baseline'>
+              <input id='ContactForm-submit-button' type="submit" value="Send" />
+              <Button className='m-2' variant="outline-dark" onClick={clearForm}>Clear</Button>
+            </div>
+            {alert &&
+              <div className="alert alert-dark" role="alert">
+                {alert}
+              </div>
+            }
           </div>
-        }
-      </div>
-    </form>
-        </Col>
-      </Row>
+        </form>
+      </Col>
+    </Row>
   );
 };
 
