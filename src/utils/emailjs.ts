abfb97a6d98e176c18b6e953e06d5797bb579dcb
jf <jpf0628@gmail.com>
2023-12-07 12:08:07 -0800

    updating env files for email.js

--- src/utils/emailjs.ts
+++ src/utils/emailjs.ts
@@ -1,29 +1,21 @@
-import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
-function Emailjs() {
-  const form: any = useRef();
+const SERVICE_ID = process.env.REACT_APP_SERVICE_KEY ?? "";
+const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID ?? "";
+const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY ?? "";
+
+function Emailjs(e:any) {
+  console.log(SERVICE_ID, TEMPLATE_ID);
+  // const form: any = useRef();
 
 //   const sendEmail = (e:any) => {
 //     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY)
       .then((result: any) => {
         console.log(result.text);
       }, (error: any) => {
         console.log(error.text);
       });
-
-//   return (
-//     <form ref={form} onSubmit={sendEmail}>
-//       <label>Name</label>
-//       <input type="text" name="user_name" />
-//       <label>Email</label>
-//       <input type="email" name="user_email" />
-//       <label>Message</label>
-//       <textarea name="message" />
-//       <input type="submit" value="Send" />
-//     </form>
-//   );
 };
 export default Emailjs
