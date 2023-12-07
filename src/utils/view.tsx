b44f8d7ebb6c51af63c9060f982523bcb9a9ddeb
jf <jpf0628@gmail.com>
2023-12-06 21:55:28 -0800

    wip: styling app

--- src/utils/view.tsx
+++ src/utils/view.tsx
@@ -2,18 +2,18 @@ import '../styles/Banner.css'
 
 function renderTypeScript(windowWidth: number, isLandscape: boolean) {
   return windowWidth > 915
-    ? <span><span className="tech">TypeScript</span>< span className="pipes" >| </span></span >
+    ? <span><span className="tech">TypeScript< span className="pipes" >|</span> </span></span >
     : <span><i className="devicon-typescript-plain" > </i><span className="pipes">|</span > </span>
 }
 function renderReact(windowWidth: number, isLandscape: boolean) {
   return windowWidth > 935
-    ? <span><span className="tech">React</span> < span className="pipes" >| </span></span >
+    ? <span><span className="tech">React< span className="pipes" >| </span></span></span >
     : <span><i className="devicon-react-original" > </i><span className="pipes">|</span > </span>
 }
 
 function renderPython(windowWidth: number, isLandscape: boolean) {
   return windowWidth > 955
-    ? <span><span className="tech">Python</span> < span className="pipes" >| </span></span >
+    ? <span><span className="tech">Python < span className="pipes" >| </span></span></span >
     : <span><i className="devicon-python-plain" > </i><span className="pipes">|</span > </span>
 }
 
